fb61049d58fb89f53cae2c49ddadef7882a5aea4
David Newlin <david.newlin@gmail.com>
2019-02-19 17:01:13 -0500

    yaml config

--- main.go
+++ main.go
@@ -1,16 +1,48 @@
 package main
 
 import (
+	"flag"
+	"io/ioutil"
+
 	"go.uber.org/zap"
+	yaml "gopkg.in/yaml.v2"
 )
 
+type config struct {
+	DevelopmentLogger bool `yaml:"DevelopmentLogger"`
+}
+
 func main() {
+	// Read flags
+	configPath := flag.String(
+		"config-file",
+		"echo.yaml",
+		"Path to yaml config file",
+	)
+	flag.Parse()
+
+	// Read config
+	data, err := ioutil.ReadFile(*configPath)
+	if err != nil {
+		panic(err)
+	}
+	cfg := &config{}
+	yaml.Unmarshal(data, cfg)
+
 	// Create logger
-	logger, err := zap.NewDevelopment()
+	var logger *zap.Logger
+	if cfg.DevelopmentLogger {
+		logger, err = zap.NewDevelopment()
+	} else {
+		logger, err = zap.NewProduction()
+	}
 	if err != nil {
 		panic(err)
 	}
 	defer logger.Sync()
 
-	logger.Info("Hello world!")
+	logger.Info(
+		"Starting up...",
+		zap.Bool("DevelopmentLogger", cfg.DevelopmentLogger),
+	)
 }
