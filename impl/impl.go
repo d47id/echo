5deff958993e3855d2b180b9d69a58277d4ae9db
David Newlin <david.newlin@gmail.com>
2019-02-24 15:20:04 -0500

    add license, basic impl

--- impl/impl.go
+++ impl/impl.go
@@ -2,20 +2,16 @@ package impl
 
 import (
 	"context"
-	"errors"
 
 	"github.com/d47id/echo/api"
 )
-
-// EchoServer is the server API for Echo service.
-// type EchoServer interface {
-// 	Shout(context.Context, *ShoutRequest) (*ShoutReply, error)
-// }
 
 // EchoImpl implements api.EchoServer
 type EchoImpl struct{}
 
 // Shout implements api.EchoServer.Shout
-func (i *EchoImpl) Shout(context.Context, *api.ShoutRequest) (*api.ShoutReply, error) {
-	return nil, errors.New("not implemented")
+func (i *EchoImpl) Shout(ctx context.Context, req *api.ShoutRequest) (*api.ShoutReply, error) {
+	return &api.ShoutReply{
+		Message: req.Message,
+	}, nil
 }
