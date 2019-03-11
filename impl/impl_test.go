e743001ea9a3b8df77910d800ddb2b73d9fb6c5f
David Newlin <david.newlin@gmail.com>
2019-03-11 19:21:12 -0400

    implement relay

--- impl/impl_test.go
+++ impl/impl_test.go
@@ -20,42 +20,27 @@ func TestEchoImpl(t *testing.T) {
 		t.Fatal(err)
 	}
 
-	//initialize bufconn
-	const bufSize = 1024 * 1024
-	lis := bufconn.Listen(bufSize)
-	defer lis.Close()
-
-	//create grpc server
-	srv := &EchoImpl{
-		Logger: l,
-	}
-	s := grpc.NewServer()
-	api.RegisterEchoServer(s, srv)
-	t.Log("starting test gRPC server")
-	go func(t *testing.T, s *grpc.Server, l net.Listener) {
-		err := s.Serve(l)
-		if err != nil {
-			t.Fatal(err)
-		}
-		t.Log("test gRPC server stopped")
-	}(t, s, lis)
-	defer s.GracefulStop()
-
-	//create grpc client
 	ctx := context.Background()
-	conn, err := grpc.DialContext(ctx, "bufnet",
-		grpc.WithDialer(getDialer(lis)),
-		grpc.WithInsecure(),
-	)
-	if err != nil {
-		t.Fatal(err)
-	}
-	defer conn.Close()
-	c := api.NewEchoClient(conn)
+	a, cancelA := startTestServer(ctx, l, nil, t)
+	defer cancelA()
+	b, cancelB := startTestServer(ctx, l, a, t)
+	defer cancelB()
 
 	t.Run("messages match", func(t *testing.T) {
 		msg := "To gRPC or not to gRPC; is that your question?"
-		reply, err := c.Shout(ctx, &api.ShoutRequest{Message: msg})
+		reply, err := a.Shout(ctx, &api.ShoutRequest{Message: msg})
+		if err != nil {
+			t.Fatal(err)
+		}
+		if reply.Message != msg {
+			t.Fatalf("unexpected mismatch. wanted %s got %s\n",
+				msg, reply.Message)
+		}
+	})
+
+	t.Run("relayed messages match", func(t *testing.T) {
+		msg := "To gRPC or not to gRPC; is that your question?"
+		reply, err := b.Shout(ctx, &api.ShoutRequest{Message: msg})
 		if err != nil {
 			t.Fatal(err)
 		}
@@ -69,5 +54,65 @@ func TestEchoImpl(t *testing.T) {
 func getDialer(l *bufconn.Listener) func(string, time.Duration) (net.Conn, error) {
 	return func(string, time.Duration) (net.Conn, error) {
 		return l.Dial()
+	}
+}
+
+func startTestServer(
+	ctx context.Context,
+	l *zap.Logger,
+	relayClient api.EchoClient,
+	t *testing.T) (
+	client api.EchoClient,
+	cancel func()) {
+
+	//initialize bufconn
+	const bufSize = 1024 * 1024
+	lis := bufconn.Listen(bufSize)
+
+	//create grpc server
+	srv := &EchoImpl{
+		Logger: l,
+		Client: relayClient,
+	}
+	s := grpc.NewServer()
+	api.RegisterEchoServer(s, srv)
+	t.Log("starting test gRPC server")
+	go func(t *testing.T, s *grpc.Server, l net.Listener) {
+		err := s.Serve(l)
+		if err != nil {
+			t.Fatal(err)
+		}
+	}(t, s, lis)
+
+	//create grpc client
+	conn, err := grpc.DialContext(ctx, "bufnet",
+		grpc.WithDialer(getDialer(lis)),
+		grpc.WithInsecure(),
+	)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	return api.NewEchoClient(conn), getCancelFunc(lis, s, conn, t)
+}
+
+func getCancelFunc(
+	lis net.Listener,
+	s *grpc.Server,
+	conn *grpc.ClientConn,
+	t *testing.T) func() {
+	return func() {
+		err := conn.Close()
+		if err != nil {
+			t.Fatal(err)
+		}
+		t.Log("client connection closed")
+		s.GracefulStop()
+		t.Log("test gRPC server stopped")
+		err = lis.Close()
+		if err != nil {
+			t.Fatal(err)
+		}
+		t.Log("bufconn listener closed")
 	}
 }
